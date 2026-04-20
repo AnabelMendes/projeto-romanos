843bfacbed385c4f2955cf9d0137ef098721cf4d
Anabel Mendes <anabelmendes.miranda@gmail.com>
2026-04-20 13:37:07 -0300

    Corrige ordem da funcao valor_romano

--- romanos.cpp
+++ romanos.cpp
@@ -1,6 +1,5 @@
 #include "romanos.hpp"
 #include <cstring>
-
 int valor_romano(char c) {
     if (c == 'I') return 1;
     if (c == 'V') return 5;
@@ -11,21 +10,23 @@ int valor_romano(char c) {
     if (c == 'M') return 1000;
     return -1;
 }
-
 int romanos_para_decimal(char const * num_romano)
 {
+    for (int i = 0; num_romano[i] != '\0'; i++) {
+        if ((num_romano[i] == 'V' || num_romano[i] == 'L' || num_romano[i] == 'D')
+            && num_romano[i] == num_romano[i + 1]) {
+            return -1;
+            }
+    }
     int total = 0;
-
     for (int i = 0; num_romano[i] != '\0'; i++) {
         int atual = valor_romano(num_romano[i]);
         int proximo = valor_romano(num_romano[i + 1]);
-
         if (proximo > atual) {
             total -= atual;
         } else {
             total += atual;
         }
     }
-
     return total;
 }
