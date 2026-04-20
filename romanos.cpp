08797ebdc62af8dfbcb28c24f815b3fca4827f70
Anabel Mendes <anabelmendes.miranda@gmail.com>
2026-04-20 12:58:34 -0300

    Implementa conversao de simbolos unicos e IV

--- romanos.cpp
+++ romanos.cpp
@@ -1,28 +1,25 @@
 #include "romanos.hpp"
 #include <cstring>
 
+int valor_romano(char c) {
+    if (c == 'I') return 1;
+    if (c == 'V') return 5;
+    if (c == 'X') return 10;
+    if (c == 'L') return 50;
+    if (c == 'C') return 100;
+    if (c == 'D') return 500;
+    if (c == 'M') return 1000;
+    return -1;
+}
+
 int romanos_para_decimal(char const * num_romano)
 {
-    if (strcmp(num_romano, "I") == 0) {
-        return 1;
+    if (strcmp(num_romano, "IV") == 0) {
+        return 4;
     }
-    if (strcmp(num_romano, "V") == 0) {
-        return 5;
-    }
-    if (strcmp(num_romano, "X") == 0) {
-        return 10;
-    }
-    if (strcmp(num_romano, "L") == 0) {
-        return 50;
-    }
-    if (strcmp(num_romano, "C") == 0) {
-        return 100;
-    }
-    if (strcmp(num_romano, "D") == 0) {
-        return 500;
-    }
-    if (strcmp(num_romano, "M") == 0) {
-        return 1000;
+
+    if (strlen(num_romano) == 1) {
+        return valor_romano(num_romano[0]);
     }
 
     return 0;
